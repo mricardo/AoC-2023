0dc5a78b7a87b305ce6ba4487d3d344ad94e8ffe
Miguel C. Ricardo <miguelcricardo@gmail.com>
2025-09-11 22:42:21 +0100

    Add files via upload

--- Day6/Day6_PartA.py
+++ Day6/Day6_PartA.py
@@ -15,11 +15,9 @@ with open("Day6/Day6_input.txt") as file:
 time = [int(e) for e in re.findall(r'\d+', lines[0])]
 distance = [int(e) for e in re.findall(r'\d+', lines[1])]
 
-print(f"Time: {time}, Distance: {distance}")
 total_records = 0
 for i in range(len(time)):
    records = calculate_records(time[i], distance[i])
-   print(f"Time: {time[i]}, Distance: {distance[i]}, Records: {records}")
    total_records = records if total_records == 0 else total_records * records
 
 print (f"Total records: {total_records}")
